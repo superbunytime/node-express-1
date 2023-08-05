67aebefe9b43812c9dd980d60d68d6fe57a6e7b9
Eric White <led.ecodyswhite@gmail.com>
2023-08-04 19:14:50 -0700

    it would help if i committed this

--- broken-app/app.js
+++ broken-app/app.js
@@ -6,10 +6,31 @@ app.use(express.json())
 
 app.get('/dev', async (request, response) => {
     try {
-        console.log(request.query);
-        const developerUsername = request.query.developer;
-        const apiResponse = await axios.get(`https://api.github.com/users/${developerUsername}`);
-        return (response.status(200).json(apiResponse.data))
+        // console.log(request.query);
+        // const developerUsername = request.query.developer;
+        // const apiResponse = await axios.get(`https://api.github.com/users/${developerUsername}`);
+        // console.log(apiResponse.data.bio)
+        // console.log(apiResponse.data.name)
+        // // return (response.status(200).json(apiResponse.data))
+        // return response.json({
+        //     bio:apiResponse.data.bio,
+        //     name:apiResponse.data.name
+        // })
+        const developerUsernames = request.query.developer;
+        const devsArray = developerUsernames.split(",")
+        const bioNameArray = []
+        console.log(devsArray)
+        for(let i = 0; i < devsArray.length; i++){
+            let apiResponse = await axios.get(`https://api.github.com/users/${devsArray[i]}`)
+            bioNameArray.push({
+                bio:apiResponse.data.bio,
+                name:apiResponse.data.name
+            })
+        }
+        console.log(bioNameArray)
+        return response.json(
+            bioNameArray
+        )
     } catch (error) {
         console.error(error.message)
         return response.status(500);
